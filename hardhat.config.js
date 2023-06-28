81d1f4cfdb1c879f5417d0e5d89ba2dd84e0ecbf
Jesse Schulman <jesse@caleos.io>
2023-06-27 22:49:16 -0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -36,7 +36,7 @@ module.exports = {
     },
     localerigon: {
       url: "http://127.0.0.1:8545",
-      wsUrl: "http://127.0.0.1:8545",
+      wsUrl: "ws://127.0.0.1:8545",
       accounts: ["0x26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48"]
     },
   },

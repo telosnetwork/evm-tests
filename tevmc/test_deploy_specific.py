1e35a0e736e44244d513033003800f1a91c40d72
Guillermo Rodriguez <guillermo@telos.net>
2023-10-18 11:08:24 -0300

    Suppress decorator warnings, showcase node_dir decorator on test_deploy_specific

--- tevmc/test_deploy_specific.py
+++ tevmc/test_deploy_specific.py
@@ -9,6 +9,7 @@ MINIMAL = ['nodeos']
 
 @pytest.mark.randomize(False)
 @pytest.mark.services(*MINIMAL)
+@pytest.mark.node_dir('tevmc/node')
 @pytest.mark.from_snapshot(200_000_000)  # start from snapshot closest to this block
 @pytest.mark.custom_subst_wasm('tevmc/contracts/eosio.evm/eosio.evm.wasm')  # use this wasm for subst
 def test_deploy_specific_mainnet(tevmc_mainnet):
